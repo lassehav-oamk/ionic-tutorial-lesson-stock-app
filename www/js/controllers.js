698c9124a5831f1ff6a563329b453ff269305f7c
Lasse Haverinen <lasse.haverinen@gmail.com>
2017-01-18 15:19:00 +0200

    Initial stock search

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -3,6 +3,12 @@ var app = angular.module('starter.controllers', []);
 app.controller('mainCtrl', function($scope, $state){
     $scope.name = "Pink Elefant";
 
+    $scope.stockData = [
+                        { name: "AAPL", value: 118 },
+                        { name: "MSFT", value: 60 },
+                        { name: "GOOG", value: 50 }
+                    ];
+
     $scope.goToSearch = function()
     {
         // change state to search
@@ -11,6 +17,27 @@ app.controller('mainCtrl', function($scope, $state){
 });
 
 
-app.controller('searchCtrl', function($scope){
+app.controller('searchCtrl', function($scope, $http){
     $scope.name = "Blue Snake";
+    $scope.input = {
+        searchText: ""
+    };
+
+    $scope.results = { name: "" };
+
+    $scope.executeSearch = function()
+    {        
+        // Execute a HTTP Request to yahoo server
+        var userInput = $scope.input.searchText;
+        $http.get('https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quotes%20where%20symbol%20in%20(%22' + userInput + '%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=')
+        .then(function(response){
+            // get some data
+            console.log(response);
+            console.log(response.data.query.results.quote.Name);
+            console.log(response.data.query.results.quote.Ask);
+
+            // display the data to the user in the view
+            $scope.results.name = response.data.query.results.quote.Name;
+        });                    
+    }
 });
